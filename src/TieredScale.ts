67247cfe74066cf1b16b96b3765f167a02ef82cb
Randall <randall@inator.biz>
2025-05-29 09:00:58 -0700

    Update tiered scale to be Int -> Data struct, not -> (Real, some-struct)
     - improves "on-chain" diagnostic logging

--- src/TieredScale.ts
+++ src/TieredScale.ts
@@ -1,29 +1,38 @@
-import { uplcDataSerializer } from "@donecollectively/stellar-contracts";
+import {
+    ContractBasedDelegate,
+    StellarContract,
+    uplcDataSerializer,
+} from "@donecollectively/stellar-contracts";
 import type { UplcData } from "@helios-lang/uplc";
 
 export type tierInfo<
     T extends string,
-    additionalTierInfo// extends Record<string, unknown>
+    additionalTierInfo // extends Record<string, unknown>
 > = {
     type: T;
     threshold: bigint;
-    scaledValue: number;
-    moreInfo: additionalTierInfo;
+    name: string;
+    scaledDetails: additionalTierInfo;
 };
 
-export type scaleInfo<
-    T extends string,
-    additionalTierInfo
-> = {
-    tiers: tierInfo<T, additionalTierInfo>[]
-}
+export type scaleInfo<T extends string, additionalTierInfo> = {
+    tiers: {
+        type: T,
+        threshold: bigint,
+        name: string,
+        scaledDetails: additionalTierInfo
+    }[];
+};
 
-export type ScaleLookupResult<
-    additionalTierInfo
-> = {
-    input: bigint;
-    found: tierInfo<any, additionalTierInfo>;
-    result: number;
+export type ScaleLookupResult<additionalTierInfo> = {
+    input: bigint | number;
+    // this is a tierInfo, inlined to ease inspectability
+    found: {
+        type: string,
+        threshold: bigint,
+        name: string,
+        scaledDetails: additionalTierInfo
+    };
     textResult: string;
 };
 
@@ -33,61 +42,56 @@ export type ScaleLookupResult<
  * "wrapped" by interpreting it in a subclass-specific way with a Cast helper
  * "unwrapped" by Uplc'ing it with a Cast helper
  * @public
- */ 
-type moreInfoUplcMap = UplcData 
+ */
+type moreInfoUplcMap = UplcData;
 
-export abstract class TieredScale<
-    T extends string,
-    additionalTierInfo
-> {
+export abstract class TieredScale<T extends string, additionalTierInfo> {
     tiers: tierInfo<T, additionalTierInfo>[];
     targetType: string;
+    hostDelegate: StellarContract<any>;
+
     constructor(
-        scaleInfo: {tiers: tierInfo<any, moreInfoUplcMap>[] },
+        scaleInfo: { tiers: tierInfo<any, moreInfoUplcMap>[] },
         targetType: T, // ??? string,
+        hostDelegate: StellarContract<any>
     ) {
         this.targetType = targetType;
-        let {tiers} = scaleInfo;
+        this.hostDelegate = hostDelegate;
+        let { tiers } = scaleInfo;
         if (!tiers.length) tiers = [];
         this.tiers = tiers
             .filter((t) => t.type === targetType)
             .sort((a, b) => Number(a.threshold - b.threshold))
-            .map((t) => (
-                {...t,
-                    moreInfo: this.wrapMoreInfo(t.moreInfo)
-                })
-            );
+            .map((t) => ({
+                ...t,
+                scaledDetails: this.wrapScaledDetails(t.scaledDetails),
+            }));
     }
 
-    wrapMoreInfo(i: moreInfoUplcMap) : additionalTierInfo {
-        return {} as additionalTierInfo
+    wrapScaledDetails(i: moreInfoUplcMap): additionalTierInfo {
+        return {} as additionalTierInfo;
     }
 
-    unwrapData() : scaleInfo<T, moreInfoUplcMap> {
+    unwrapData(): scaleInfo<T, moreInfoUplcMap> {
         const scaleInfo = this.tiers.map((scaleEntry) => {
-            const { 
-                type, 
-                threshold, 
-                scaledValue, 
-                moreInfo 
-            } = scaleEntry;
-            
+            const { type, threshold, name, scaledDetails } = scaleEntry;
+
             return {
                 type,
                 threshold,
-                scaledValue,
-                moreInfo: this.unwrapMoreInfo(moreInfo)
-            } 
+                name,
+                scaledDetails: this.unwrapScaledDetails(scaledDetails),
+            };
         });
 
-        return {tiers: scaleInfo }
+        return { tiers: scaleInfo };
     }
 
-    abstract unwrapMoreInfo(
-        i: additionalTierInfo
-    ) : moreInfoUplcMap 
+    abstract unwrapScaledDetails(i: additionalTierInfo): moreInfoUplcMap;
 
-    findTierValue(value: bigint): ScaleLookupResult<additionalTierInfo> {
+    findTierValue(
+        value: number | bigint
+    ): ScaleLookupResult<additionalTierInfo> {
         let found = this.tiers.findLast((s) => BigInt(value) >= s.threshold);
         if (!found) {
             throw new Error(
@@ -95,17 +99,18 @@ export abstract class TieredScale<
             );
         }
         // console.log("found tier value: ", found.scaledValue);
-        const result = {
+        const result: ScaleLookupResult<additionalTierInfo> = {
             input: value,
-            found: { ...found },
-            result: found.scaledValue,
-            textResult: this.UiPresentation(found.scaledValue),
+            found,
+            textResult: this.UiPresentation(found.scaledDetails),
         };
-        
-        console.log("tier lookup: "+ uplcDataSerializer("tierLookup", result, 1));
 
-        return result
+        console.log(
+            "tier lookup: " + uplcDataSerializer("tierLookup", result, 1)
+        );
+
+        return result;
     }
-    abstract UiPresentation(value: number): string;
-    abstract fromUIValue(value: string): number;
+    abstract UiPresentation(details: additionalTierInfo): string;
+    // abstract fromUIValue(value: string): additionalTierInfo;
 }
