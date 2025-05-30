d6c21f303069febe37aa37a79d15470f094f2e0a
Randall <randall@inator.biz>
2025-05-30 12:34:57 -0700

    Helper for reducing boilerplate for typed makeSTokMintDelegateBundle

--- src/STokMintDelegate.hlb.ts
+++ src/STokMintDelegate.hlb.ts
@@ -1,6 +1,6 @@
-import { MintSpendDelegateBundle } from "@donecollectively/stellar-contracts"
+import { CapoHeliosBundle, MintSpendDelegateBundle, type ConcreteCapoDelegateBundle } from "@donecollectively/stellar-contracts"
 import type { Source } from "@helios-lang/compiler-utils"
-import stellarBaseMintDelegate from "./STokMintDelegate.hl"
+import tokenomicsBasicMintDelegate from "./STokMintDelegate.hl"
 import StellarTokenomicsCapoBundle from "./StellarTokenomics.hlb.js";
 
 /**
@@ -11,7 +11,7 @@ export class STokMintDelegateBundle
 extends MintSpendDelegateBundle.usingCapoBundleClass(
     StellarTokenomicsCapoBundle
 ) {
-    specializedDelegateModule = stellarBaseMintDelegate
+    specializedDelegateModule = tokenomicsBasicMintDelegate
     requiresGovAuthority = true
 
     get delegateName() {
@@ -21,3 +21,56 @@ extends MintSpendDelegateBundle.usingCapoBundleClass(
 }
 
 export default STokMintDelegateBundle;
+
+type IsStokMintDelegate = {
+    specializedDelegateModule: typeof tokenomicsBasicMintDelegate
+    requiresGovAuthority: true
+}
+
+export type Constructor<T> = new (...args: any[]) => T;
+/**
+ * Creates a typed helper class to use as a mint delegate for tokenomics subclasses
+ * 
+ * ## Usage:
+ * In your `MyMintDelegate.hlb.ts`:
+ * ```typescript
+ *    export const MyMintDelegate = makeSTokMintDelegateBundle(MyCapoBundle, "MyMintDelegate")
+ *    export default MyMintDelegate
+ * ```
+ * This HLB file will be compiled to make  `.bridge.ts` and `.typeInfo.ts` for your delegate.  Import
+ * the DataBridge and make your `MySpendMintDelegate.ts`, extending `STokMintDelegate` 
+ * and using:
+ * ```typescript
+ * export class MyMintSpendDelegate extends STokMintDelegate {
+ *    get delegateName() {
+ *        return "MyMintSpendDelegate";
+ *    }
+ *    dataBridgeClass = MyMintSpendDelegateDataBridge;
+ *     scriptBundle(): CapoDelegateBundle {
+ *        return MyMintSpendDelegateBundle.create();
+ *     }
+ * ```
+ * 
+ * Use your Mint Delegate class in your Capo:
+ * ```typescript
+ * delegateRoles({
+ *    spendDelegate: defineRole("spendDgt", MyMintSpendDelegate, {}),
+ *    mintDelegate: defineRole("mintDgt", MyMintSpendDelegate, {}),
+ * })
+ * ```
+ * 
+ * @public
+ */
+export function makeSTokMintDelegateBundle<CapoBundleType extends typeof CapoHeliosBundle>(
+    capoBundle: CapoBundleType,
+    delegateName: string
+) : Constructor<STokMintDelegateBundle & IsStokMintDelegate> & ConcreteCapoDelegateBundle {
+    const base = STokMintDelegateBundle.usingCapoBundleClass(capoBundle)
+    abstract class SpecificDelegateBundle extends base {
+        specializedDelegateModule = tokenomicsBasicMintDelegate
+        requiresGovAuthority = true
+        delegateName = delegateName
+    }
+    return SpecificDelegateBundle as any
+    // Constructor<STokMintDelegateBundle & IsStokMintDelegate>
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { StellarTokenomicsCapo } from "./StellarTokenomicsCapo.js";
+export { STokMintDelegate } from "./STokMintDelegate.js";
+export { makeSTokMintDelegateBundle } from "./STokMintDelegate.hlb.js";
 export * from "./MarketSale/index.js";
 
 // export { FundedPurposeController } from "./FundedPurpose/FundedPurposeController.js"
